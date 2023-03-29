b686264ffb92cd73f147103496e4260dd5698797
wangshoutai <shoutai.wang@lotuscars.com.cn>
2023-03-29 13:57:23 +0800

    47

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 echo building...
-npm -v & npm --prefix ./packages/addon/ i & npm --prefix ./packages/addon/ run build & npm --prefix ./packages/app/ i & npm --prefix ./packages/app/ run build
+npm -v & npm --prefix ./packages/addon/ i & npm --prefix ./packages/addon/ run build & npm --prefix ./packages/app/ i & npm --prefix ./packages/app/ run make
