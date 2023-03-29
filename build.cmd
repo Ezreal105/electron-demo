76572f2d2fe0b032311954c6c191523c501e5d05
wangshoutai <shoutai.wang@lotuscars.com.cn>
2023-03-29 14:49:34 +0800

    61

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 echo building...
 npm -v &^
-set GYP_MSVS_VERSION='2019' &^
+set GYP_MSVS_VERSION=2019 &^
 npm --prefix ./packages/addon/ i &^
 npm --prefix ./packages/addon/ run build &^
 npm --prefix ./packages/app/ i &^
-npm --prefix ./packages/app/ run make 
+set GYP_MSVS_VERSION=2019 & npm --prefix ./packages/app/ run make 
